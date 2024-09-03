f583b2bb543e7e451a02c17b1efd61e5a8b48a5d
Lionel Traore <hello@lioneltraore.com>
2024-09-03 00:47:05 +0000

    add react project: mortgage-repayment-calculator

---
+++ apps/mortgage-repayment-calculator/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'mortgage-repayment-calculator',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/mortgage-repayment-calculator',
+};
