6ba01f7a6ae4672deb69fa93680ca78b1e72f77e
Lionel Traore <hello@lioneltraore.com>
2024-09-08 12:34:31 +0000

    add preline

--- apps/expenses-chart-component/src/app/app.component.ts
+++ apps/expenses-chart-component/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { IStaticMethods } from 'preline/preline';
+
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 @Component({
   selector: 'frontend-challenge-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          window.HSStaticMethods?.autoInit();
+        }, 100);
+      }
+    });
+  }
 }
--- apps/expenses-chart-component/tailwind.config.js
+++ apps/expenses-chart-component/tailwind.config.js
@@ -6,9 +6,22 @@ module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
+    '../../node_modules/preline/preline.js',
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 };
